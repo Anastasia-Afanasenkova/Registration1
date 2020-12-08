c31bf3a563aaedc383fc86334e12b84249d9cb5f
Anastasia Afanasenkova <timosha020702@yandex.ru>
2020-12-09 00:33:43 +0300

    first version

---
+++ AuthorizationWPF/Manager.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuthorizationWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Manager.xaml
+    /// </summary>
+    public partial class Manager : Page
+    {
+        public MainWindow mWindow;
+        public Manager(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mWindow = mainWindow;
+        }
+    }
+}
---
+++ AuthorizationWPF/UserForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuthorizationWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для UserForm.xaml
+    /// </summary>
+    public partial class UserForm : Page
+    {
+        public MainWindow mWindow;
+        public UserForm(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mWindow = mainWindow;
+        }
+    }
+}
